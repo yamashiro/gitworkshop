2673c8971e6a4aad0902ca6c4e30edef924011c5
yamashiro <ysy0217@gmail.com>
2012-09-02 15:01:47 +0900

    Fizzになる条件が2になっていたのを3にした

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -5,7 +5,7 @@ if ($val >= 100) {
   echo '*Too big*';
 } elseif ($val % 3 == 0 && $val % 5 == 0) {
   echo '*FizzBuzz*';
-} elseif ($val % 2 == 0) {
+} elseif ($val % 3 == 0) {
   echo '*Fiz*';
 } elseif ($val % 5 == 0) {
   echo '*Buzz*';
